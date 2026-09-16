a127f5a6b6ce1bda62bf440c6d4d1e308d2c1ceb
Arluru <aracelicarolina.montero@gmail.com>
2026-09-16 13:55:35 -0300

    Intento de interfaz de interacción con el entorno
    En progreso

--- Assets/Scripts/Collect Objects.cs
+++ Assets/Scripts/Collect Objects.cs
@@ -7,11 +7,28 @@ public enum ItemID
 
 public class CollectObjects : MonoBehaviour, ICollectable
 {
+    [SerializeField] private Character character;
     [SerializeField] private ItemID id;
     public ItemID ID => id;
 
     public void Collect()
     {
-        
+        if(character.interacting == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+   void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && character.interacting == true)
+        {
+            Collect();
+        }
     }
 }
