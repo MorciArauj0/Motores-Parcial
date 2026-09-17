b53f9e509c84391441a4c96e727f9cf66712b2e6
Aaron <araujo0708.ar@gmail.com>
2026-09-17 00:22:18 -0300

    inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    Character character;
+    public event Action<Inventory> OnInventoryChange;
+
+    [SerializeField] private List<ItemID> InventoryList = new List<ItemID>();
+
+    public void OnTriggerStay(Collider other)
+    {
+        CollectObjects item = other.gameObject.GetComponent<CollectObjects>();
+        if (item != null && character.IsInteracting == true)
+        {
+            Debug.Log("al iventario");
+            InventoryList.Add(item.ID);
+            //Destroy(other.gameObject);
+            //OnInventoryChange.Invoke(???);
+            //Una variable que indique el estado de la lista
+        }
+    }
+
+    void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+
+}
