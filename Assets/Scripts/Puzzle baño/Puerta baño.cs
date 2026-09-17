661293ed5881c818ba6638838e3f55231fcdcaa2
Aaron <araujo0708.ar@gmail.com>
2026-09-16 21:19:20 -0300

    arreglando rotacion de puertas cuando abren

--- Assets/Scripts/Puzzle baño/Puerta baño.cs
+++ Assets/Scripts/Puzzle baño/Puerta baño.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
 
 public class Puertabaño : MonoBehaviour, IInteractable
 {
+    public float openAngle = 90f;
+
     [SerializeField] private GameObject pivot;
+    private Quaternion openRotation;
+    private Quaternion closedRotation;
+
 
     public void Interact()
     {
-        transform.Rotate(new Vector3(0, -80, 0));
+        openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+        Debug.Log ("Se abrio");
     }
 }
